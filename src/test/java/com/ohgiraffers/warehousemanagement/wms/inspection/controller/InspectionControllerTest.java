f3e349fb64fdbe91613bd9aa3c9f980855621a20
smg0218 <smg000218@gmail.com>
2025-04-28 16:30:29 +0900

    test: 일반 조회 및 태그로 조회 테스트 완료

--- src/test/java/com/ohgiraffers/warehousemanagement/wms/inspection/controller/InspectionControllerTest.java
+++ src/test/java/com/ohgiraffers/warehousemanagement/wms/inspection/controller/InspectionControllerTest.java
@@ -4,6 +4,7 @@ import com.ohgiraffers.warehousemanagement.wms.inspection.model.common.Inspectio
 import com.ohgiraffers.warehousemanagement.wms.inspection.model.common.InspectionTransactionType;
 import com.ohgiraffers.warehousemanagement.wms.inspection.model.dto.request.InspectionRequestDTO;
 import com.ohgiraffers.warehousemanagement.wms.inspection.model.dto.response.InspectionResponseDTO;
+import com.ohgiraffers.warehousemanagement.wms.inspection.model.entity.Inspection;
 import com.ohgiraffers.warehousemanagement.wms.inspection.service.InspectionServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,5 +40,40 @@ class InspectionControllerTest {
         log.info(inspection.toString());
 
         assertNotNull(inspection, "등록에 성공해야 합니다!");
+    }
+
+    @Test
+    @DisplayName("메인 페이지 조회 테스트")
+    void getAllInspections() {
+        Page<Inspection> allInspection = inspectionService.getAllInspection(1, 10);
+
+        log.info("Inspection\n{}", allInspection.getContent());
+
+        assertTrue(allInspection.getContent().size() <= 10, "페이징한 개수는 10개보다 작아야 합니다.");
+    }
+
+    @Test
+    @DisplayName("태그로 조회 테스트")
+    void getAllTagInspections() {
+        Page<Inspection> allTagInspection = inspectionService.getAllInspectionByTag("INSPECTION", 1, 10);
+
+        log.info("Inspection\n{}", allTagInspection.getContent());
+
+        assertTrue(allTagInspection.getContent().size() <= 10, "페이징한 개수는 10개보다 작아야 합니다.");
+    }
+
+    @Test
+    @DisplayName("다중 등록 테스트")
+    void createMultiInspection() {
+        for(int i = 1; i <= 43; i++) {
+            InspectionRequestDTO dto = new InspectionRequestDTO(1, null, InspectionTransactionType.INSPECTION, i, i, 0, InspectionStatus.OK);
+
+
+            InspectionResponseDTO inspection = inspectionService.createInspection(dto);
+
+            log.info(inspection.toString());
+
+            assertNotNull(inspection, "등록에 성공해야 합니다!");
+        }
     }
 }
