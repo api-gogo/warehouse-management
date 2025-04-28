931ef8ba6717c0528b698f39c997e601cc33bd63
dang-woo <kdw3917ss@gmail.com>
2025-04-28 21:50:48 +0900

    :memo: shipment 레파지토리 생성

--- src/main/java/com/ohgiraffers/warehousemanagement/wms/shipment/repository/ShipmentRepository.java
+++ src/main/java/com/ohgiraffers/warehousemanagement/wms/shipment/repository/ShipmentRepository.java
@@ -3,5 +3,6 @@ package com.ohgiraffers.warehousemanagement.wms.shipment.repository;
 import com.ohgiraffers.warehousemanagement.wms.shipment.model.entity.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ShipmentRepository extends JpaRepository<Shipment, Integer> {
 }
