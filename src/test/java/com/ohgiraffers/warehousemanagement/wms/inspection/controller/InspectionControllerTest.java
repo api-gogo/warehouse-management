ba33657e46f428f53985a72d051cc0018c044deb
smg0218 <smg000218@gmail.com>
2025-04-28 13:42:31 +0900

    test: 검수 추가 테스트 완료

---
+++ src/test/java/com/ohgiraffers/warehousemanagement/wms/inspection/controller/InspectionControllerTest.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.warehousemanagement.wms.inspection.controller;
+
+import com.ohgiraffers.warehousemanagement.wms.inspection.model.common.InspectionStatus;
+import com.ohgiraffers.warehousemanagement.wms.inspection.model.common.InspectionTransactionType;
+import com.ohgiraffers.warehousemanagement.wms.inspection.model.dto.request.InspectionRequestDTO;
+import com.ohgiraffers.warehousemanagement.wms.inspection.model.dto.response.InspectionResponseDTO;
+import com.ohgiraffers.warehousemanagement.wms.inspection.service.InspectionServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback
+class InspectionControllerTest {
+    private static final Logger log = LoggerFactory.getLogger(InspectionControllerTest.class);
+    private final InspectionServiceImpl inspectionService;
+
+    @Autowired
+    public InspectionControllerTest(InspectionServiceImpl inspectionService) {
+        this.inspectionService = inspectionService;
+    }
+
+    @Test
+    @DisplayName("등록 테스트")
+    void createInspection() {
+        InspectionRequestDTO dto = new InspectionRequestDTO(1, null, InspectionTransactionType.INSPECTION, 50, 50, 0, InspectionStatus.OK);
+
+        InspectionResponseDTO inspection = inspectionService.createInspection(dto);
+
+        log.info(inspection.toString());
+
+        assertNotNull(inspection, "등록에 성공해야 합니다!");
+    }
+}
