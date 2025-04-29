b31d58834d38401796e99c295c5afc79655de2df
Kang <ktbisboy99@gmail.com>
2025-04-29 11:12:38 +0900

    feat: getLogUserByUserId()로 로그용 유저 조회 추가

---
+++ src/main/java/com/ohgiraffers/warehousemanagement/wms/user/model/dto/LogUserDTO.java
@@ -0,0 +1,60 @@
+package com.ohgiraffers.warehousemanagement.wms.user.model.dto;
+
+public class LogUserDTO {
+
+    private Integer userId;
+    private String userName;
+    private String userPart;
+    private String userRole;
+    private String userStatus;
+
+    public LogUserDTO() {}
+
+    public LogUserDTO(Integer userId, String userName, String userPart, String userRole, String userStatus) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userPart = userPart;
+        this.userRole = userRole;
+        this.userStatus = userStatus;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPart() {
+        return userPart;
+    }
+
+    public void setUserPart(String userPart) {
+        this.userPart = userPart;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+}
