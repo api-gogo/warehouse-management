89e37208eb602d8fb1aa6b9965d23cc3981c1ab6
Kang <ktbisboy99@gmail.com>
2025-04-29 18:02:48 +0900

    fix: 오타 수정

--- src/main/java/com/ohgiraffers/warehousemanagement/wms/store/controller/StoreController.java
+++ src/main/java/com/ohgiraffers/warehousemanagement/wms/store/controller/StoreController.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Component
+@Controller
 @RequestMapping("/stores")
 public class StoreController {
 
