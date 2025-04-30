737ca3a7be445a399a2ff1a27e45b65db2c6e84e
Kang <ktbisboy99@gmail.com>
2025-04-30 16:50:13 +0900

    fix: SecurityConfig 권한 수정

--- src/main/java/com/ohgiraffers/warehousemanagement/wms/auth/config/SecurityConfig.java
+++ src/main/java/com/ohgiraffers/warehousemanagement/wms/auth/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
 
             // ==================== 업무 도메인별 접근 권한 ==================== //
 
-            // 5. 전체 조회, 상세 조회 - 개발 중이므로 일시적으로 모든 사용자에게 허용
+            // 5. 전체 조회, 상세 조회 - 재직중인 사원은 모두 볼 수 있음.
             auth.requestMatchers("/storages", "/storages/{id}").permitAll();
             auth.requestMatchers("/shipments", "/shipments/{id}").permitAll();
             auth.requestMatchers("/inventories", "/inventories/{id}").permitAll();
@@ -81,6 +81,8 @@ public class SecurityConfig {
             auth.requestMatchers("/purchases", "/purchases/{id}").permitAll();
             auth.requestMatchers("/sales", "/sales/{id}").permitAll();
             auth.requestMatchers("/products", "/products/{id}").permitAll();
+            auth.requestMatchers("/suppliers", "/suppliers/{id}").permitAll();
+            auth.requestMatchers("/stores", "/stores/{id}").permitAll();
 
             // 6. 각 부서별 작업에 대한 권한 설정 -> 이후 직책별에 맞게 수정 필요
             // 입고 부서
@@ -94,9 +96,9 @@ public class SecurityConfig {
             // 반품 부서
             auth.requestMatchers("/returns/**").hasAnyAuthority("반품_사원", "반품_매니저", "반품_관리자", "통합_관리자");
             // 발주 부서
-            auth.requestMatchers("/purchases/**").hasAnyAuthority("발주_사원", "발주_매니저", "발주_관리자", "통합_관리자");
+            auth.requestMatchers("/purchases/**", "/suppliers/**").hasAnyAuthority("발주_사원", "발주_매니저", "발주_관리자", "통합_관리자");
             // 수주 부서
-            auth.requestMatchers("/sales/**").hasAnyAuthority("수주_사원", "수주_매니저", "수주_관리자", "통합_관리자");
+            auth.requestMatchers("/sales/**", "/stores/**").hasAnyAuthority("수주_사원", "수주_매니저", "수주_관리자", "통합_관리자");
             // 상품 부서
             auth.requestMatchers("/products/**").hasAnyAuthority("상품_사원", "상품_매니저", "상품_관리자", "통합_관리자");
 
